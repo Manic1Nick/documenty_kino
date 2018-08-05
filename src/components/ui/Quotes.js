fe1c0125e99d12bef36ad85a72259a7a6a09a047
Nick <nick.no.cave@gmail.com>
2018-08-05 10:54:26 +0300

    change Quotes block, styles;

--- src/components/ui/Quotes.js
+++ src/components/ui/Quotes.js
@@ -1,27 +1,83 @@
 import { Component } from 'react'
+import { PropTypes } from 'prop-types'
 import { Link } from 'react-router-dom'
+import classNames from 'classnames'
 
 export default class Quotes extends Component {
+    
+    constructor() {
+        super()
+        this.state = {
+            postsWithQuotes: [],
+            activePostIndex: 0,
+            animate: false
+        }
+    }
 
+    componentWillMount = () => {
+        let postsWithQuotes = this.context.posts.filter(post => post.quote.length > 0)
+
+        this.setState({
+            postsWithQuotes,
+            activePostIndex: Math.floor(Math.random() * postsWithQuotes.length)
+        })
+    }
+
+    componentWillUnmount = () => {
+        clearInterval(this.timerID)
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.changeActivePostIndex(),
+            10000
+        )
+        this.activeAnimate()
+    }
+
+    changeActivePostIndex = () => {
+        let lastIndex = this.state.activePostIndex,
+            activePostIndex
+
+        while (!activePostIndex || activePostIndex === lastIndex)
+            activePostIndex = Math.floor(Math.random() * this.state.postsWithQuotes.length)
+
+        this.setState({ activePostIndex })
+        this.activeAnimate()
+    }
+
+    activeAnimate() {
+        this.setState({ animate: true })
+        
+		setTimeout(() => {
+			this.setState({ animate: false })
+		}, 1000)
+    }
+ 
     render() {
-        const { posts } = this.props,
-            postsWithQuotes = posts.filter(post => post.quote.length > 0),
-            post = postsWithQuotes[0]
+        const { postsWithQuotes, activePostIndex, animate } = this.state,
+            post = postsWithQuotes[activePostIndex]
+
+        if (!post) return null
+
+        let classQuoteText = classNames('quote-text', { fadeIn: animate })
         
         return(
             <div className='Quotes'>
                 <ion-icon name="quote"></ion-icon>
 
-                <div className='quote-text'>
-                    {post.quote}
-                </div> 
+                <div className={ classQuoteText }>{ post.quote }</div> 
 
                 <button className='quote-link'>
-                    <Link to={`/${post.tag}/${post.id}`}>
+                    <Link to={`/${post.tag}/${post.id}`} title={ post.title }>
                         Читати статтю
                     </Link>
                 </button>
             </div>
         )
     }
+}
+
+Quotes.contextTypes = {
+    posts: PropTypes.array
 }
